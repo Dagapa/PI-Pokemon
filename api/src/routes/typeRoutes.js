f8718d1d075b67f2a28a4fff8ed18bec61d290e9
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-30 14:03:25 -0500

    typesroute

---
+++ api/src/routes/typeRoutes.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const express = require("express");
+const { Type } = require("../db");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const type = await axios.get("https://pokeapi.co/api/v2/type");
+  const res = type.data;
+  try {
+    let types = res.results.map((element) => element.name);
+    types.forEach((type) => {
+      Type.findOrCreate({
+        where: {
+          name: type,
+        },
+      });
+    });
+    const allTypes = await Type.findAll();
+    return res.status(200).send(allTypes);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+module.exports = router;
