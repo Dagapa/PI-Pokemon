ee0ada813caa8529a3f56663c1645f80ada1fdf0
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-01-30 18:03:12 -0500

    first component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import NavBar from './componets/NavBar/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Henry Pokemon</h1>
     </div>
   );
---
+++ client/src/componets/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return(
+    <nav>
+      <h1>Hola soy una nav</h1>
+    </nav>
+  )
+}
+
+export default NavBar;
