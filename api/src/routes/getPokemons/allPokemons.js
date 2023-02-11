b4453a104093732123954fa2e16d4937d9bf84ab
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-02-10 19:02:30 -0500

    fig a bugs 1.2

--- api/src/routes/getPokemons/allPokemons.js
+++ api/src/routes/getPokemons/allPokemons.js
@@ -4,9 +4,7 @@ const dbInfo = require("./db");
 const getAllPokemon = async () => {
   const apiPoke = await apiInfo();
   const dbPoke = await dbInfo();
-  console.log(dbPoke.length, apiPoke.length);
   return apiPoke.concat(dbPoke);
-  // return [...apiPoke,...dbPoke];
 };
 
 module.exports = getAllPokemon;
--- api/src/routes/getPokemons/api.js
+++ api/src/routes/getPokemons/api.js
@@ -5,37 +5,34 @@ const apiInfo = async () => {
     let initialURL = "https://pokeapi.co/api/v2/pokemon?limit=40";
     let pokemons = [];
 
-    // limit to 40 pokemons
-    while (initialURL != null && pokemons.length < 40) {
-      let res = await axios.get(initialURL);
-      let pokemonsData = res.data;
-      let tempPokemons = pokemonsData.results.map((element) => ({
-        name: element.name,
-        url: element.url,
-      }));
-      pokemons.push(...tempPokemons);
-      initialURL = pokemonsData.next;
+    let res = await axios.get(initialURL);
+    let pokemonsData = res.data;
+    let tempPokemons = pokemonsData.results.map((element) => ({
+      name: element.name,
+      url: element.url,
+    }));
+    pokemons.push(...tempPokemons);
+    initialURL = pokemonsData.next;
 
-      //subrequest Data Pokemons
-      let pokesWithData = await Promise.all(
-        pokemons.map(async (element) => {
-          let pokemon = await axios.get(element.url);
-          return {
-            id: pokemon.data.id,
-            name: pokemon.data.name,
-            img: pokemon.data.sprites.other.home.front_default,
-            types: pokemon.data.types.map(item => item),
-            hp: pokemon.data.stats[0].base_stat,
-            attack: pokemon.data.stats[1].base_stat,
-            defense: pokemon.data.stats[2].base_stat,
-            speed: pokemon.data.stats[5].base_stat,
-            height: pokemon.data.height,
-            weight: pokemon.data.weight,
-          };
-        })
-      );
-      return pokesWithData;
-    }
+    //subrequest Data Pokemons
+    let pokesWithData = await Promise.all(
+      pokemons.map(async (element) => {
+        let pokemon = await axios.get(element.url);
+        return {
+          id: pokemon.data.id,
+          name: pokemon.data.name,
+          img: pokemon.data.sprites.other.home.front_default,
+          types: pokemon.data.types.map((item) => item),
+          hp: pokemon.data.stats[0].base_stat,
+          attack: pokemon.data.stats[1].base_stat,
+          defense: pokemon.data.stats[2].base_stat,
+          speed: pokemon.data.stats[5].base_stat,
+          height: pokemon.data.height,
+          weight: pokemon.data.weight,
+        };
+      })
+    );
+    return pokesWithData;
   } catch (err) {
     console.error(err);
   }
